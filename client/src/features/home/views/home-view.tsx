5365f19185a417ff3a184a9b99c16f01eb628580
HuynhLinh <hql.0812.dlp@gmail.com>
2026-05-20 17:44:00 +0700

    ♻️ refactor(client): compose home with new sections

--- client/src/features/home/views/home-view.tsx
+++ client/src/features/home/views/home-view.tsx
@@ -1,22 +1,24 @@
 import { PricingSection } from '@/components/usable/pricing-table';
 import { HeroSection } from '../components/hero-section';
+import { ServiceMarquee } from '../components/service-marquee';
 import { ServicesSection } from '../components/services-section';
 import { UspSection } from '../components/usp-section';
 import { ProcessSection } from '../components/process-section';
 import { GallerySection } from '../components/gallery-section';
-import { ProjectsTeaser } from '../components/projects-teaser';
+import { TestimonialsSection } from '../components/testimonials-section';
 import { ContactSection } from '../components/contact-section';
 
 export function HomeView() {
   return (
     <>
       <HeroSection />
+      <ServiceMarquee />
       <ServicesSection />
       <UspSection />
       <PricingSection />
       <ProcessSection />
       <GallerySection />
-      <ProjectsTeaser />
+      <TestimonialsSection />
       <ContactSection />
     </>
   );
