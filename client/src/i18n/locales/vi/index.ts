83dcdf855b3a29e9ca258d540d7ac5488bf2e8d2
HuynhLinh <hql.0812.dlp@gmail.com>
2026-05-20 17:44:00 +0700

    ✨ feat(client): add Vietnamese locale barrel

---
+++ client/src/i18n/locales/vi/index.ts
@@ -0,0 +1,19 @@
+import common from './common.json';
+import site from './site.json';
+import home from './home.json';
+import services from './services.json';
+import pricing from './pricing.json';
+import contact from './contact.json';
+import projects from './projects.json';
+import meta from './meta.json';
+
+export default {
+  common,
+  site,
+  home,
+  services,
+  pricing,
+  contact,
+  projects,
+  meta,
+};
